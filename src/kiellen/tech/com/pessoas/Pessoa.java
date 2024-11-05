22181c16b5438b4e8cf2a3ad9916f63af1ca6c89
tomazpantadasilvaneto <tomazpanta@hotmail.com>
2024-11-04 22:08:03 -0300

    Atualizações 04/10/2024

--- src/kiellen/tech/com/pessoas/Pessoa.java
+++ src/kiellen/tech/com/pessoas/Pessoa.java
@@ -1,5 +1,6 @@
 package kiellen.tech.com.pessoas;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.time.LocalDate;
 import java.time.Period;
@@ -19,20 +20,27 @@ public class Pessoa {
     private Scanner leitor = new Scanner(System.in);
 
     public void cadastrar() {
-        System.out.println("Informe o nome: ");
-        this.nome = leitor.nextLine();
-        System.out.println("== Informe a data de nascimento == ");
-        System.out.println("Informe o dia: ");
-        int dia = leitor.nextInt();
-        System.out.println("Informe o mês: ");
-        int mes = leitor.nextInt();
-        System.out.println("Informe o ano: ");
-        int ano = leitor.nextInt();
-        this.dataNascimento = LocalDate.of(ano, mes, dia);
-        this.endereco = new Endereco();
-        this.endereco.cadastrar();
-        this.telsContato = new Telefone();
-        this.telsContato.cadastrar();
+        try {
+            System.out.println("Informe o nome: ");
+            this.nome = leitor.nextLine();
+            System.out.println("== Informe a data de nascimento == ");
+            System.out.println("Informe o dia: ");
+            int dia = leitor.nextInt();
+            System.out.println("Informe o mês: ");
+            int mes = leitor.nextInt();
+            System.out.println("Informe o ano: ");
+            int ano = leitor.nextInt();
+            this.dataNascimento = LocalDate.of(ano, mes, dia);
+            this.endereco = new Endereco();
+            this.endereco.cadastrar();
+            this.telsContato = new Telefone();
+            this.telsContato.cadastrar();
+
+        } catch (InputMismatchException e){
+            System.out.println("Valor informado está incorreto, informe um inteiro");
+            this.cadastrar();
+        }
+
     }
 
     public int obterIdade(){
